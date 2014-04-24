e31bd883530093cb164702ea4c989870ddc6ba52
Ilya S <public.ilya.s@gmail.com>
2014-04-24 23:06:29 +0700

    refactor Jenny

--- src/Jenny/Jenny.php
+++ src/Jenny/Jenny.php
@@ -11,41 +11,41 @@ use Jenny\Evaluator\ExpressionBuilder;
 class Jenny {
 
     /**
-     * The Tokenizer instance
+     * The Tokenizer instance.
      *
-     * @var \Jenny\Lexer\Tokenizer
+     * @var Lexer\Tokenizer
      */
     protected $tokenizer;
 
     /**
-     * The Evaluator instance
+     * The Evaluator instance.
      *
-     * @var \Jenny\Evaluator\Evaluator
+     * @var Evaluator\Evaluator
      */
     protected $evaluator;
 
     /**
-     * The constructor
+     * The constructor.
      *
-     * @param  \Jenny\Lexer\Tokenizer|null     $tokenizer
-     * @param  \Jenny\Evaluator\Evaluator|null $evaluator
+     * @param  Lexer\Tokenizer|null $tokenizer
+     * @param  Evaluator\Evaluator|null $evaluator
      * @return Jenny
      */
-    public function __construct(Tokenizer $tokenizer = null,
-        Evaluator $evaluator = null)
+    public function __construct(
+        Tokenizer $tokenizer = null,
+        Evaluator $evaluator = null
+    )
     {
         $this->tokenizer = $tokenizer ?: new Tokenizer(new Reader, new Analyzer);
 
-        $container = new Container;
-
         $this->evaluator = $evaluator ?:
-            new Evaluator($container, new ExpressionBuilder($container));
+            new Evaluator($container, new ExpressionBuilder(new Container));
     }
 
     /**
-     * Run the given line of code
+     * Run the given line of code.
      *
-     * @param  string $code
+     * @param string $code
      * @return string
      */
     public function run($code)
